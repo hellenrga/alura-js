c0a8bf97b73041ca3b41041b1b1fd49c2f660f07
hellenrga <hellenassis.dev@gmail.com>
2023-04-20 20:56:09 -0300

    number type

---
+++ type-number.js
@@ -0,0 +1,23 @@
+// tipo Number
+
+const myNumber = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const MatOperator = num1 - num2;
+
+console.log(MatOperator)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante = .5;
+
+const newOperation = num1 / numeroPontoFlutuante;
+
+console.log(newOperation)
+
+const alura = "Alura"
+
+console.log(alura * num1)
