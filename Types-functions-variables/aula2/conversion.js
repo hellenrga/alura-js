7a90c7f6947583e52807289559694fa630a1f780
hellenrga <hellenassis.dev@gmail.com>
2023-04-20 22:56:26 -0300

    conversão de tipos, texto de conclusão

---
+++ Types-functions-variables/aula2/conversion.js
@@ -0,0 +1,14 @@
+//tipo de dado
+//boleano
+
+//conversao implicita
+const numero = 456;
+const numeroString = Number("456")
+
+
+//conversao explicita
+
+// Number()
+// String()
+
+console.log(numero + numeroString)
