e8a5a2616057ec0ad8e8012217900e607fad6f02
hellenrga <hellenassis.dev@gmail.com>
2023-04-20 21:17:13 -0300

    concat and strings

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const text1 = "Hello, world"
+const text2 = 'Hello, world'
+const password = "safepassword456##"
+const NumberString = "345678910"
+
+const quote = "My name is "
+const nameIs = "Hellen"
+
+// concat (+)
+
+console.log(quote + nameIs)
+
+//template string or literal
