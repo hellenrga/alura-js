271fb542fac67febcb045a5d2695a3c17f9d3e2b
hellenrga <hellenassis.dev@gmail.com>
2023-08-16 13:48:32 -0300

    commit

--- js-arrays/aula1/testeFixo.js
+++ js-arrays/aula1/testeFixo.js
@@ -10,11 +10,38 @@ for (let i = 0, j = 0; i < lista.length; i++, j++) {
     console.log(lista[i] + j)
 } */
 
-const lista = [1,2,3,4,5];
+/* const lista = [1,2,3,4,5];
 let soma = 0;
 
 for (let elemento of lista) {
     soma += elemento;
 }
 
-console.log(soma)
+console.log(soma) */
+
+
+/* const nome = ["Alura"]
+
+const nomeUpper = nome.map((alura) => {
+    return alura.toUpperCase();
+})
+
+console.log(nomeUpper)  
+
+const nome = "Alura";
+let nomeMaiusculas = "";
+
+for (let i = 0; i < nome.length; i++) {
+ nomeMaiusculas += nome[i].toUpperCase()
+}
+console.log(nomeMaiusculas) */
+
+const num = [1,2,3,4]
+
+function xDez(num) {
+    return num * 10
+}
+
+const arraySomada = num.map(xDez)
+
+console.log(arraySomada)
